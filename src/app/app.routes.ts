181538d7e68f6d76d0627da5f3badb52e0b99660
karthikraghu <karthiksoraghuram@gmail.com>
2026-01-01 23:56:05 +0100

    Refactor routing, remove portfolio component, and enhance home page layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  },
-  {
-    path: 'portfolio',
-    component: PortfolioComponent
   },
   {
     path: '**',
